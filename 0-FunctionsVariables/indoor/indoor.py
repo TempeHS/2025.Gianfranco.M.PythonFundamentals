051a5ba1a442da06c30c74f11874dc964b42bb60
frnaky <124547280+frnaky@users.noreply.github.com>
2024-02-06 22:39:14 +0000

    hello added, directory restructure

--- 0-FunctionsVariables/indoor/indoor.py
+++
@@ -1,5 +0,0 @@
-# ask user for name
-name = input('whats ur name? ')
-
-# print output
-print("hello, " + name)
---
+++ 0-FunctionsVariables/vidtests/hello.py
@@ -0,0 +1,5 @@
+# ask for name remove spaces and capitalsier
+name = input("whats ur name? ").strip().title()
+
+# say hello
+print("hello, " + name)
