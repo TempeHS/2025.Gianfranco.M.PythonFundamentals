5f703c25b231ee72b9ea7e3c0e2b6b2a633da2b6
frnaky <124547280+frnaky@users.noreply.github.com>
2024-02-14 02:51:34 +0000

    upload interpreter.py

--- 1-Conditionals/interpreter/interpreter.py
+++ 1-Conditionals/interpreter/interpreter.py
@@ -1,8 +1,11 @@
+number_1 = float(input("first number: "))
+number_1 = round(number_1, 1)
+number_2 = float(input("second number: "))
+number_2 = round(number_2, 1)
+
+
 def calculate():
     operation = input("what operator? (+ , - , *, /) ")
-
-    number_1 = int(input("first number: "))
-    number_2 = int(input("second number: "))
 
     if operation == "+":
         print("{} + {} = ".format(number_1, number_2))
@@ -14,7 +17,7 @@ def calculate():
 
     elif operation == "*":
         print("{} * {} = ".format(number_1, number_2))
-        print(number_1 * number_2)
+        print(number_1 * number_2, 2)
 
     elif operation == "/":
         print("{} / {} = ".format(number_1, number_2))
