fe40b1aa4252d1243b076e1929ff07d59e1c90b4
frnaky <124547280+frnaky@users.noreply.github.com>
2024-02-14 07:29:03 +0000

    meal.py donegit add -A!

--- 1-Conditionals/meal/meal.py
+++ 1-Conditionals/meal/meal.py
@@ -1,17 +1,20 @@
-def meal():
-    time = input("whats the time prick? ")
-
-
-meal()
+def mealtime():
+    meal = input("whats the time prick?! ")
+    meal = convert(meal)
+    if 7 <= meal <= 8:
+        print("it is breakfast time !")
+    elif 12 <= meal <= 13:
+        print("it is lunch time !")
+    elif 18 <= meal <= 19:
+        print("it is dinner time !")
+    else:
+        print("it is nothing time !")
 
 
 def convert(time):
-    hour, minute = time.split(":")
-    time = float(hour) + float(minute) / 60
+    hours, minutes = time.split(":")
+    time = float(hours) + float(minutes) / 60
     return time
 
 
-time = convert(meal)
-
-
-print(time)
+mealtime()
