462496572a88bb1a05b4f7f7decbe8ca7a6d93ff
frnaky <124547280+frnaky@users.noreply.github.com>
2024-02-23 03:16:00 +0000

    rps.py done

---
+++ 2-Loops/camel/camel.py
@@ -0,0 +1,1 @@
+var = input("what is ur varbaiee name??")
--- inClassWork/rps.py
+++ inClassWork/rps.py
@@ -1,35 +1,42 @@
 import random
 
-playerChoice = input("CHOSE YOUR CHARACTER ")
+gamesPlayed = 0
+gamesWon = 0
 
+while gamesPlayed < 3:
+    playerChoice = input("CHOSE YOUR CHARACTER ")
 
-mylist = ["rock", "paper", "scissors"]
-rand = random.choices(mylist)
+    mylist = ["rock", "paper", "scissors"]
+    rand = random.choice(mylist)
 
+    gamesPlayed += 1
 
-rand()
+    match playerChoice:
+        case "rock":
+            if rand == "rock":
+                print("DRAW")
+            if rand == "paper":
+                print("YOU LOSE")
+            if rand == "scissors":
+                print("YOU WIN !")
+                gamesWon += 1
 
-match playerChoice:
-    case "rock":
-        if rand == "rock":
-            print("DRAW")
-        if rand == "paper":
-            print("YOU LOSE")
-        if rand == "scissors":
-            print("YOU WIN !")
+        case "paper":
+            if rand == "rock":
+                print("you win")
+                gamesWon += 1
+            if rand == "paper":
+                print("draw")
+            if rand == "scissors":
+                print("you lose")
 
-    case "paper":
-        if rand == "rock":
-            print("you win")
-        if rand == "paper":
-            print("draw")
-        if rand == "scissors":
-            print("you lose")
+        case "scissors":
+            if rand == "rock":
+                print("you lose")
+            if rand == "paper":
+                print("winner !!")
+                gamesWon += 1
+            if rand == "scissors":
+                print("draw")
 
-    case "scissors":
-        if rand == "rock":
-            print("you lose")
-        if rand == "paper":
-            print("winner !!")
-        if rand == "scissors":
-            print("draw")
+print(f"gamesWon)
