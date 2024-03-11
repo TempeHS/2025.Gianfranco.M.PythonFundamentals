2a528e063f44369c00c11b1ab3edd80b182b5237
frnaky <124547280+frnaky@users.noreply.github.com>
2024-03-11 01:35:36 +0000

    forgot about plates.py lol

--- 2-Loops/plates/plates.py
+++ 2-Loops/plates/plates.py
@@ -1,13 +1,27 @@
 def main():
-    plate = input("Plate: ")
+    plate = input("what ur plate mna !!!.>>: ")
     if is_valid(plate):
-        print("Valid")
+        print("haha i dmv accept !! good job man")
     else:
-        print("Invalid")
+        print("you are declined")
 
 
-def is_valid(s): 
-    
+def is_valid(s):
+    if 2 <= len(s) <= 6 and s.isalnum():
+        if s.isalpha():
+            return True
+        else:
+            if s[:2].isalpha() and s[-2:].isdigit():
+                for i in range(len(s)):
+                    if s[i].isdigit():
+                        if s[i].startswith("0") or s[i:].isalpha():
+                            return False
+                        else:
+                            return True
+            else:
+                return False
+    else:
+        return False
 
 
 main()
